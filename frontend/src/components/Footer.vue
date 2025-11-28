c70633175b36c6be145b901425892b804f1689dd
i-am-nitish <142775050+i-am-nitish@users.noreply.github.com>
2025-11-29 02:26:43 +0530

    Fix Leaflet map marker

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -16,6 +16,15 @@
 
 <script>
 import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix Leaflet's default icon paths (Vite doesn't bundle them automatically)
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
 
 export default {
   name: "Footer",
